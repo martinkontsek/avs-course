0dcf4b0bf525550e7671690dd41e831ed41731ff
Martin Kontsek <martinkontsek@gmail.com>
2026-04-08 09:28:06 +0000

    CV07: TCP server single thread.

--- cv07/src/tcp-server.c
+++ cv07/src/tcp-server.c
@@ -1,34 +1,19 @@
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include <strings.h>
+#include <stdint.h>
 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netinet/ip.h>
 #include <netinet/tcp.h>
 #include <arpa/inet.h>
 
-#include <pthread.h>
-
 #define IP "::"
-#define PORT 4200
-#define BACKLOG 2
-
-void * handle_client(void *sock)
-{
-    int client_sock = *((int *)sock);
-    char buffer[1024];
-    bzero(buffer, sizeof(buffer));
-    read(client_sock, buffer, sizeof(buffer));
-
-    printf("Msg: %s\n",
-        buffer
-    );
-
-    close(client_sock);
-}
+#define PORT 9999
+#define BACKLOG 1
 
 int main()
 {
@@ -41,15 +26,15 @@ int main()
     }
 
     struct sockaddr_in6 addr;
-    bzero(&addr, sizeof(addr));
+    memset(&addr, 0, sizeof(addr));
     addr.sin6_family = AF_INET6;
-    addr.sin6_port = htons(PORT);
-    if(inet_pton(AF_INET6, IP, &addr.sin6_addr) < 1)
+    if(inet_pton(AF_INET6, IP, &addr.sin6_addr) <= 0)
     {
         perror("INET_PTON");
         close(sock);
         exit(EXIT_FAILURE);
-    } 
+    }
+    addr.sin6_port = htons(PORT);
 
     if(bind(sock, (struct sockaddr *)&addr, sizeof(addr)) == -1)
     {
@@ -67,27 +52,36 @@ int main()
 
     int client_sock;
     socklen_t addr_len = sizeof(addr);
-    char buffer[1024];
     for(;;)
     {
-        bzero(&addr, sizeof(addr));
+        memset(&addr, 0, sizeof(addr));
         client_sock = accept(sock, (struct sockaddr *)&addr, &addr_len);
         if(client_sock == -1)
         {
             perror("ACCEPT");
             continue;
         }
-        
-        bzero(buffer, sizeof(buffer));
-        inet_ntop(AF_INET6, &addr.sin6_addr, buffer, sizeof(buffer));
-
-        printf("Connected client [%s]:%d \n",
-            buffer,
+        //FFFF:FFFF:
+        char string_ip6[50];
+        inet_ntop(AF_INET6, &addr.sin6_addr, string_ip6, 50);
+        printf("Connected client [%s:%d].\n",
+            string_ip6,
             ntohs(addr.sin6_port)
         );
 
-        pthread_t thread_id;
-        pthread_create(&thread_id, NULL, handle_client, &client_sock);
+        char buffer[100];
+        for(;;)
+        {
+            if(recv(client_sock, buffer, 100, 0) <= 0)
+            {
+                close(client_sock);
+                break;
+            }
+
+            printf("MSG: %s\n",
+                buffer
+            );
+        }
     }
 
     close(sock);
