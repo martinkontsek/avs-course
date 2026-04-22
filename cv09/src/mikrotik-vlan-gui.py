a040aa331dff3e322a507e5db1f2468c223c19a5
Martin Kontsek <martinkontsek@gmail.com>
2026-04-22 10:07:16 +0000

    CV9: Mikrotik VLAN GUI.

--- cv09/src/mikrotik-vlan-gui.py
+++ cv09/src/mikrotik-vlan-gui.py
@@ -1,79 +1,73 @@
 #!/usr/bin/env python3
 
 import requests as r
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 
-HOST = "http://192.168.1.3"
+HOST = "http://192.168.1.3/rest"
 USER = "admin"
 PASS = ""
-BRIDGE = "switch"
-
-web_gui = Flask(__name__)
 
 def get_switch_ifaces():
-    resp = r.get(HOST+"/rest/int/br/port", auth=(USER,PASS))
+    resp = r.get(HOST+"/int/bri/port", auth=(USER, PASS))
 
     if resp.status_code == 200:
         out = list()
         for iface in resp.json():
-            out.append({".id": iface[".id"], "interface": iface["interface"], "pvid": iface["pvid"]})
+            out.append({"id": iface[".id"],
+                        "name": iface["interface"],
+                        "pvid": iface["pvid"]
+                       })
         return out
+    else:
+        print(resp.status_code)
+        return None
     
+def get_iface_id(iface_name):
+    for iface in get_switch_ifaces():
+        if iface["name"] == iface_name:
+            return iface["id"]
     return None
 
-def set_pvid(iface, pvid):
-    ifaces = get_switch_ifaces()
-    for i in ifaces:
-        if i["interface"] == iface:
-            body = {"pvid": pvid}
-            print(body)
-            resp = r.patch(HOST+"/rest/int/br/port/"+i[".id"], auth=(USER,PASS), json=body)
-            
-            if resp.status_code == 200:
-                return True
-    return False
 
-def add_vlan(vlan_id):
-    body = {"bridge": BRIDGE, "vlan-ids": vlan_id}
-    resp = r.put(HOST+"/rest/int/br/vlan", auth=(USER,PASS), json=body)
-
-    if resp.status_code == 201:
+def set_iface_pvid(iface_name, pvid):
+    id = get_iface_id(iface_name)
+    if id == None:
+        return False
+    
+    body = {"pvid": pvid}
+    resp = r.patch(HOST+"/int/bri/port/"+id, auth=(USER, PASS), json=body)
+    if resp.status_code == 200:
         return True
-    return False
+    else:
+        print(resp.status_code)
+        print(resp.json())
+        return False
 
-def get_vlan():
-    resp = r.get(HOST+"/rest/int/br/vlan", auth=(USER,PASS))
 
-    if resp.status_code == 200:
-        out = list()
-        for iface in resp.json():
-            out.append({".id": iface[".id"], "vlan-ids": iface["vlan-ids"], "tagged": iface["tagged"]})
-        return out
-    return None
+gui = Flask(__name__)
 
-@web_gui.route("/", methods=["GET", "POST"])
+@gui.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
+    if request.method == "GET":
+        ifaces = get_switch_ifaces()
+        return render_template("vlan.html", iface_list=ifaces)
+    else:
         print(request.form.keys())
-        for key in request.form.keys():
-            port = key.split("-")[0]
-            type_pvid = key.split("-")[1]
-            if type_pvid == "pvid":
-                set_pvid(port, request.form[key])
-    
-    
-    out = get_switch_ifaces()
-    vids = get_vlan()
-    for iface in out:
-        iface["type"] = "access"
+        for name in request.form.keys():
+            out = set_iface_pvid(name, request.form.get(name))
+            if out == False:
+                ifaces = get_switch_ifaces()
+                return render_template("vlan.html", iface_list=ifaces, status=False)
+        
+        ifaces = get_switch_ifaces()
+        return render_template("vlan.html", iface_list=ifaces, status=True)
 
-    return render_template("vlan.html", ifaces=out, vids=vids)
 
 
 if __name__ == "__main__":
-    # out = get_switch_ifaces() 
-    # out = add_vlan(2)
-    # out = get_vlan()
-    # print(out)
-    web_gui.run(host="0.0.0.0", port="8080")
-    # set_pvid("e1", 1)
+    # ifaces = get_switch_ifaces()
+    # print(ifaces)
+
+    # set_iface_pvid("e2", 10)
+
+    gui.run(host="0.0.0.0", port=9999)
